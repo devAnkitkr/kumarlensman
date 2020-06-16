70e7f0dd188c8a16d6d1ea5c5664cbd0052355c8
notheera <itsmeankit12feb@gmail.com>
2020-06-17 02:07:28 +0530

    added https instead of http in fetch

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -39,7 +39,7 @@ const Contact = () => {
     const handleForm = (e) => {
         e.preventDefault()
         const { name, email, message } = e.target
-        fetch('http://68.183.90.34:4000/send', {
+        fetch('https://68.183.90.34:4000/send', {
             method: 'POST',
             headers: { "Content-type": "application/json" },
             body: JSON.stringify({
